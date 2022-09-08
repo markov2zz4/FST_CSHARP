8833c34690706b52f97c838529ac1a32112dcc08
SHpetAI <shpetai@DOM201.LOCAL>
2022-09-08 14:29:00 +0700

    08_09 SKopin

---
+++ Threads_1/Threads_1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+
+object locker = new object();
+long counter = 100;
+
+void WriteSecond()
+{
+    
+    for (int i = 0; i < 20; i++)
+    {
+        
+        //lock (locker)
+        {
+            counter = 0;
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine($"{new string(' ', 10)}Secondary: {counter}" );
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+            Thread.Sleep(10);
+        }
+    }
+      
+}
+ThreadStart writeSecond = WriteSecond;
+Thread thread = new Thread(writeSecond);
+thread.Start();
+
+
+for (int i = 0; i < 20; i++)
+{
+    //lock (locker)
+    {
+        counter = 100;
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine($"Primary: {counter}");
+        Console.ForegroundColor = ConsoleColor.Gray;
+
+        Thread.Sleep(10);
+    }
+}
+
+
---
+++ Threads_2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+
+Thread thread1 = new Thread(() =>
+{
+    while (true) Console.WriteLine("Primary");
+});
+
+thread1.IsBackground = true;
+thread1.Start();
+
+Thread.Sleep(10000);
+
+/*
+ * Сделать 
+ */
