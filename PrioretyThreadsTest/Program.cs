0d8503b5215feb7cb2dd14fd631c785c9a6f6b68
shpetai <shpetai@402D-13>
2022-09-14 14:35:59 +0700

    PriorityThreads 14.09 +

---
+++ PrioretyThreadsTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace PrioretyThreadsTest
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            object obj = new object();
+            Writer writer = new Writer('-');
+            Writer writer2 = new Writer('+');
+            Thread thread2 = new Thread(writer2.WriteChars)
+            {
+                Priority = ThreadPriority.Highest
+            };
+            Thread.CurrentThread.Priority = ThreadPriority.Lowest;
+            thread2.Start();
+            writer.WriteChars();
+            Console.WriteLine($"\nПервичный поток написал {writer.Counter} -");
+            Console.WriteLine($"\nВторичный поток написал {writer2.Counter} +");
+            //Console.WriteLine($"\nПотоки написали {Writer.GlobalCounter} символов");
+            Console.WriteLine($"\nПотоки написали {Interlocked.Read(ref Writer.GlobalCounter)} символов");
+            
+        }
+    }
+
+}
---
+++ PrioretyThreadsTest/Writer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace PrioretyThreadsTest
+{
+
+    public class Writer
+    {
+        public static long GlobalCounter = 0;
+        private const int Span = 2*1000 ;
+        private char _symbol;
+        private readonly Stopwatch timeInterval = new Stopwatch();
+        public int Counter { get; private set; }
+
+        public Writer(char symbol)
+        {
+            this._symbol = symbol;
+        }
+
+        public void WriteChars()
+        {
+            timeInterval.Restart();
+            do
+            {
+                //Console.Write(_symbol);
+                Counter++;
+                Interlocked.Increment(ref GlobalCounter);
+            }
+            while (timeInterval.ElapsedMilliseconds < Span);
+        }
+
+    }
+
+}
