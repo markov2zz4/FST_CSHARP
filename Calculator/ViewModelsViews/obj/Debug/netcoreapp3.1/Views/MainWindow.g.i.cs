1dcb7969595acc33f6ac89b2f68f8abccc518fa1
shpetai <shpetai@402D-13>
2022-09-14 14:23:21 +0700

    fixed bugs & add ThreadPriority 14.09

--- Calculator/ViewModelsViews/obj/Debug/netcoreapp3.1/Views/MainWindow.g.i.cs
+++ Calculator/ViewModelsViews/obj/Debug/netcoreapp3.1/Views/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace ViewModelsViews.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/ViewModelsViews;V1.0.0.0;component/views/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/ViewModelsViews;component/views/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
