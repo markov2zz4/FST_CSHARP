5081b5c59c398fc140024f10b97e737035dd98e5
MarkoW <albertnov@internet.ru>
2022-11-09 13:46:21 +0700

    ____

---
+++ SpinLock/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+
+namespace SpinLock_1
+{
+    // C#  
+    public class MyType
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SpinLock/Spinlock_01.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace SpinLock
+{
+    class Spinlock_01
+    {
+        private SpinLock _spinLock = new SpinLock();
+
+        public void DoWork()
+        {
+            bool lockTaken = false;
+            try
+            {
+                _spinLock.Enter(ref lockTaken);
+                // do work here protected by the lock  
+            }
+            finally
+            {
+                if (lockTaken) _spinLock.Exit();
+            }
+        }
+    }
+}
